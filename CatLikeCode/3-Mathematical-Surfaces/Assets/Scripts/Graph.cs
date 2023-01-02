340fd29337313d230f7176dc10f3ea2a6093539c
HenrickyL <Henrickyl1@gmail.com>
2023-01-02 00:25:22 -0300

    end 2 building graph

--- CatLikeCode/3-Mathematical-Surfaces/Assets/Scripts/Graph.cs
+++ CatLikeCode/3-Mathematical-Surfaces/Assets/Scripts/Graph.cs
@@ -7,20 +7,29 @@ public class Graph : MonoBehaviour
    Transform pointPrefab;
    [SerializeField,Range(10, 100)]
    int resolution = 10;
-   List<Transform> points = new List<Transform>();
+   Transform[] points;
 
    void Awake(){
+    points = new Transform[resolution];
     float step = 2f/resolution;
     var scale = Vector3.one*step;
     Vector3 position = Vector3.zero;
-    for(var i=0; i<resolution; i++){
-        Transform point = Instantiate(pointPrefab);
+    for(var i=0; i<points.Length; i++){
+        Transform point = points[i]  = Instantiate(pointPrefab);
         point.SetParent(transform, false);
         position.x = (i+0.5f)*step-1f;
-        position.y = position.x * position.x;
         point.localPosition = position;
         point.localScale = scale;
-        points.Add(point);
     }
+   }
+
+   void Update(){
+    float time =  Time.time;
+        for(var i=0; i<points.Length; i++){
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x +time));
+            point.localPosition = position;
+        }
    }
 }
